1482c1e64488f643dfa79d9af1231c8139ff875f
Diego Rocha <diegors@rede.ulbra.br>
2022-07-14 23:20:15 -0300

    injeção de dependencia no CustomerService

--- Exercicio1/Services/CustomerService.cs
+++ Exercicio1/Services/CustomerService.cs
@@ -1,10 +1,22 @@
 using Exercicio1.Domain.Entities;
+using Exercicio1.Domain.Interfaces.RepositoryInterfaces;
 using Exercicio1.Domain.Interfaces.ServiceInterfaces;
 
 namespace Exercicio1.Services
 {
     public class CustomerService : ICustomerService
     {
+        private readonly ICustomerRepository _customerRepository;
+        private readonly IUnitOfWork _unitOfWork;
+
+        public CustomerService(
+            ICustomerRepository customerRepository,
+            IUnitOfWork unitOfWork)
+        {
+            this._customerRepository = customerRepository;
+            this._unitOfWork = unitOfWork;
+        }
+
         public Task<bool> AddCustomerAsync(Customer customer)
         {
             throw new NotImplementedException();
