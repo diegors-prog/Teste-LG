fdca080acb39be6cb20ca2b24ff56266d0373de1
Diego Rocha <diegors@rede.ulbra.br>
2022-07-15 00:36:19 -0300

    tratamento de erros e padronizando retornos

--- Exercicio1/Domain/DTOs/ResultDTO.cs
+++ Exercicio1/Domain/DTOs/ResultDTO.cs
@@ -1,7 +1,29 @@
 namespace Exercicio1.Domain.DTOs
 {
-    public class ResultDTO
+    public class ResultDTO<T>
     {
-        
+        public ResultDTO(T data, List<string> errors)
+        {
+            Data = data;
+            Errors = errors;
+        }
+
+        public ResultDTO(T data)
+        {
+            Data = data;
+        }
+
+        public ResultDTO(List<string> errors)
+        {
+            Errors = errors;
+        }
+
+        public ResultDTO(string error)
+        {
+            Errors.Add(error);
+        }
+
+        public T Data { get; private set; }
+        public List<string> Errors { get; private set; } = new List<string>();
     }
 }
---
+++ Exercicio1/Extensions/ModelStateExtension.cs
@@ -0,0 +1,22 @@
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+
+namespace Exercicio1.Extensions
+{
+    public static class ModelStateExtension
+    {
+        public static List<string> GetErrors(this ModelStateDictionary modelState)
+        {
+            var result = new List<string>();
+
+            foreach (var item in modelState.Values)
+            {
+                foreach (var error in item.Errors)
+                {
+                    result.Add(error.ErrorMessage);
+                }
+            }
+
+            return result;
+        }
+    }
+}
