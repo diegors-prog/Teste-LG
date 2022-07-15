04c6afd1dfb32240683fb2fc0496bf825a35033c
Diego Rocha <diegors@rede.ulbra.br>
2022-07-14 23:11:48 -0300

    feat: migrations e criação do database

---
+++ Exercicio1/Migrations/20220715020934_InitialMigrations.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Exercicio1.Migrations
+{
+    public partial class InitialMigrations : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "customer",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    email = table.Column<string>(type: "VARCHAR", maxLength: 80, nullable: false),
+                    name = table.Column<string>(type: "VARCHAR", maxLength: 80, nullable: false),
+                    phone_number = table.Column<string>(type: "VARCHAR", maxLength: 20, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_customer", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "contact",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    relationship_type = table.Column<long>(type: "INTEGER", nullable: false),
+                    customer_id = table.Column<int>(type: "INTEGER", nullable: false),
+                    name = table.Column<string>(type: "VARCHAR", maxLength: 80, nullable: false),
+                    phone_number = table.Column<string>(type: "VARCHAR", maxLength: 20, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_contact", x => x.id);
+                    table.ForeignKey(
+                        name: "FK_Contact_Customer",
+                        column: x => x.customer_id,
+                        principalTable: "customer",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_contact_customer_id",
+                table: "contact",
+                column: "customer_id");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "contact");
+
+            migrationBuilder.DropTable(
+                name: "customer");
+        }
+    }
+}
